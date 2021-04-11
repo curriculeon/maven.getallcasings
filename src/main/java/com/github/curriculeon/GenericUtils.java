1c095e783653ba3511738d1173d2bd18815eeb30
git-leon <xleonhunter@gmail.com>
2021-04-11 17:44:38 -0400

    gutted implementation

--- src/main/java/com/github/curriculeon/GenericUtils.java
+++ src/main/java/com/github/curriculeon/GenericUtils.java
@@ -1,8 +1,6 @@
 package com.github.curriculeon;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 public class GenericUtils {
     /**
@@ -11,11 +9,6 @@ public class GenericUtils {
      * @return array with contents identical to `collection`
      */
     public static  <T> T[] toArray(Collection<T> collection) {
-        List<T> list = new ArrayList<>(collection);
-        T[] toR = (T[]) new Object[collection.size()];
-        for (int i = 0; i < list.size(); i++) {
-            toR[i] = list.get(i);
-        }
-        return toR;
+        return null;
     }
 }
--- src/main/java/com/github/curriculeon/StringUtils.java
+++ src/main/java/com/github/curriculeon/StringUtils.java
@@ -1,9 +1,5 @@
 package com.github.curriculeon;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
-
 public class StringUtils {
     /**
      * @param string - string to be evaluated
@@ -11,12 +7,7 @@ public class StringUtils {
      * @return near-identical string whose characters at specified indices are capitalized
      */
     public static String upperCaseIndices(String string, Integer... indices) {
-        String temp1 = string;
-        for (Integer index : indices) {
-            Character character = string.charAt(index);
-            temp1 = replaceAtIndex(temp1, Character.toUpperCase(character), index);
-        }
-        return temp1;
+        return null;
     }
 
 
@@ -27,7 +18,7 @@ public class StringUtils {
      * @return near-identical string with `valueToBeInserted` inserted at `index`
      */
     public static String insertAtIndex(String stringToBeManipulated, String valueToBeInserted, Integer index) {
-        return stringToBeManipulated.substring(0, index) + valueToBeInserted + stringToBeManipulated.substring(index);
+        return null;
     }
 
     /**
@@ -37,8 +28,6 @@ public class StringUtils {
      * @return near-identical string with character at `index` replaced with `replacementValue`
      */
     public static String replaceAtIndex(String stringToBeManipulated, Character replacementValue, Integer index) {
-        StringBuilder sb = new StringBuilder(stringToBeManipulated);
-        sb.setCharAt(index, replacementValue);
-        return sb.toString();
+        return null;
     }
 }
